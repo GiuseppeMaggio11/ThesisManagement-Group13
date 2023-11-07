3d15ee7cccb31aa18e36c062dd27ef44d3f8e260
Manuel Fissore <manuel.fissore.37@gmail.com>
2023-11-07 19:01:50 +0100

    first commit

--- server/dao.js
+++ server/dao.js
@@ -2,6 +2,7 @@
 
 //const crypto = require("crypto");
 const mysql = require("mysql2");
+const crypto = require("crypto");
 
 // open the database
 const dbConfig = {
@@ -11,6 +12,152 @@ const dbConfig = {
   database: "db_se_thesismanagement",
 };
 const connection = mysql.createConnection(dbConfig);
+
+exports.getUser = (email, password) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM users WHERE email = ?";
+    connection.query(sql, [email], (error, results, fields) => {
+      if (error) {
+        console.error("Errore nella query getUser:", error);
+        reject(error);
+      } else if (results.length === 0) {
+        resolve(false);
+      } else {
+        const userRow = results[0];
+        const user = {
+          username: userRow.email,
+          user_type: userRow.user_type_id,
+        };
+        const salt = userRow.salt;
+        crypto.scrypt(password, salt, 64, (err, hashedPassword) => {
+          if (err) reject(err);
+          const passwordHex = Buffer.from(userRow.password, "hex");
+
+          if (!crypto.timingSafeEqual(passwordHex, hashedPassword))
+            resolve(false);
+          else resolve(user);
+        });
+      }
+    });
+  });
+};
+
+exports.getUserByEmail = (email) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM users WHERE email = ?";
+    connection.query(sql, [email], (error, results, fields) => {
+      if (error) {
+        console.error("Errore nella query getUserByEmail:", error);
+        reject(error);
+      } else if (results.length === 0) {
+        resolve({ error: "User not found." });
+      } else {
+        const userRow = results[0];
+        const user = {
+          username: userRow.email,
+          user_type: userRow.user_type_id,
+        };
+        resolve(user);
+      }
+    });
+  });
+};
+
+exports.getUserID = (username) => {
+  return new Promise((resolve, reject) => {
+    if (!username) {
+      reject({ error: "parameter is missing" });
+    }
+    const sql = "SELECT * FROM student WHERE email = ?";
+    connection.query(sql, [username], (error, results, fields) => {
+      if (error) {
+        console.error("Errore nella query getUserIDByEmail:", error);
+        reject(error);
+      } else if (results.length === 0) {
+        reject({ error: "User not found." });
+      } else {
+        const userRow = results[0];
+        resolve(userRow.id);
+      }
+    });
+  });
+};
+
+const isThesisValid = async (thesisID) => {
+  if (!thesisID) {
+    throw { error: "parameter is missing" };
+  }
+  const sql =
+    "SELECT * FROM thesis WHERE id = ? AND expiration > NOW() AND is_archived = FALSE";
+  try {
+    const results = await connection.execute(sql, [thesisID]);
+
+    if (results.length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  } catch (error) {
+    console.error("Error in the query:", error);
+    throw error;
+  }
+};
+
+const isAlreadyExisting = async (studentID, thesisID) => {
+  if (!thesisID || !studentID) {
+    throw { error: "parameter is missing" };
+  }
+
+  const sql =
+    "SELECT COUNT(*) as count FROM application WHERE student_id = ? AND thesis_id = ?";
+  try {
+    const results= await connection.execute(sql, [studentID, thesisID]);
+    console.log(results)
+    if (results.count != 0) {
+      return true; 
+    } else {
+      return false; 
+    }
+  } catch (error) {
+    console.error("Error in the query:", error);
+    throw error;
+  }
+};
+
+
+
+// Function to create a new application
+exports.newApply = async (studentID, ThesisID) => {
+  const status = "pending";
+
+  try {
+    const isValid = await isThesisValid(ThesisID);
+    const existing = await isAlreadyExisting(studentID, ThesisID);
+    if (!isValid) {
+      throw new Error( "The thesis does not exist or is not active");
+    }
+    if (existing) {
+      throw new Error("You are already applied for this thesis");
+    }
+    const query =
+      "INSERT INTO application (student_id, thesis_id, status, application_date) VALUES (?, ?, ?, ?)";
+    const values = [studentID, ThesisID, status, new Date()];
+    try {
+      const result = await connection.execute(query, values);
+      console.log(result);
+      return result;
+    } catch (error) {
+      if (error.code === "ER_DUP_ENTRY") {
+        throw new Error("You have already applied to this thesis.");
+      } else {
+        throw error;
+      }
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
 
 process.on("exit", () => {
   console.log("Closing db connection");
--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@
 
 const express = require("express");
 const morgan = require("morgan");
+const passport = require("passport");
 const { check, validationResult, body } = require("express-validator");
 const dao = require("./dao");
 const cors = require("cors");
+const multer = require('multer');
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+
 
 const app = express();
 const port = 3001;
@@ -18,7 +23,125 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.static("public"));
 
+app.use(
+  session({
+    secret: "hjsojsdjndhirheish",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    dao.getUser(username, password).then((user) => {
+      if (!user)
+        return done(null, false, {
+          message: "Incorrect username and/or password.",
+        });
+
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.username);
+});
+
+passport.deserializeUser((username, done) => {
+  dao
+    .getUserByEmail(username)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => {
+      done(err, null);
+    });
+});
+
+
+const storage = multer.diskStorage({
+  destination: 'curricula/',
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9); //change this with studentidname_curricula
+    const extension = file.originalname.split('.').pop(); // Get the file extension
+    const filename = `${uniqueSuffix}.${extension}`;
+    cb(null, filename);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    const filetypes = /pdf/; // Allow only PDF files
+    const mimetype = filetypes.test(file.mimetype);
+    if (mimetype) {
+      return cb(null, true);
+    } else {
+      cb("Error: Solo file PDF sono ammessi!");
+    }
+  }
+});
+
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  return res.status(401).json({ error: "Not authenticated" });
+};
+
+const isProfessor = (req, res, next) => {
+  if (req.isAuthenticated() && req.user.user_type === "PROF") return next();
+  return res.status(401).json({ error: "Not professor" });
+};
+
+const isStudent = (req, res, next) => {
+  if (req.isAuthenticated() && req.user.user_type === "STUD") return next();
+  return res.status(401).json({ error: "Not student" });
+};
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 /***USER - API***/
+// login
+app.post("/api/session/login", function (req, res, next) {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      return res.status(401).json(info);
+    }
+    req.login(user, (err) => {
+      if (err) return next(err);
+
+      return res.json(req.user);
+    });
+  })(req, res, next);
+});
+
+// logout
+app.delete("/api/session/logout", (req, res) => {
+  req.logout(() => {
+    res.end();
+  });
+});
+
+app.get("/api/session/userinfo", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+  } else res.status(401).json({ error: "Unauthenticated user!" });
+});
+
+//body => thesis_id
+app.post('/api/newApply', isStudent, [check('airplane_id').isInt()], upload.single('file'), async (req, res) => {
+  const thesis_id = req.body.thesis_id
+  try {
+    const userID = await dao.getUserID(req.user.username); // await the Promise to resolve
+    console.log(userID)
+    const result = await dao.newApply(userID, thesis_id);
+    res.status(200).send('Application created successfully'); // Send a success response
+  } catch (error) {
+    res.status(500).send(error+' '); // Send an error response with status code 500
+  }
+});
 
 /***API***/
 
