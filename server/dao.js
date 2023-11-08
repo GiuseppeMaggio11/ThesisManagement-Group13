43e5c035d07847eb1bd1a7fb287b618cb860fefe
Jacopo Corrias <Corrias.jacopo@gmail.com>
2023-11-08 16:31:36 +0100

    Backend new thesis, todo cosupervisor after db fix

--- server/dao.js
+++ server/dao.js
@@ -66,3 +66,215 @@ process.on("exit", () => {
   console.log("Closing db connection");
   connection.end();
 });
+
+// Creates a new thesis row in thesis table, must receive every data of thesis, returns newly created row, including autoicremented id ( used to add new rows in successive tables)
+exports.createThesis = (thesis) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'INSERT INTO thesis (title, description, supervisor_id, thesis_level, type_name, required_knowledge, notes, expiration, cod_degree, is_archived) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?); ';
+    connection.query(sql, [thesis.title, thesis.description, thesis.supervisor_id, thesis.thesis_level, thesis.type_name, 
+                  thesis.required_knowledge, thesis.notes, thesis.expiration, thesis.cod_degree, thesis.is_archived], function (err, rows) {
+      if (err) {
+        reject(err);
+      }
+      const thesisrow = { id: rows.insertId, ...thesis}
+      resolve(thesisrow);
+    });
+  });
+};
+
+// Inserts new row in type_table, must receive type not currently present in table, returns newly inserted type
+exports.createThesis_type = (type) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'INSERT INTO type_table (name) VALUES (?)';
+    connection.query(sql, [type], function (err) {
+      if (err) {
+        reject(err);
+      }
+      resolve(type);
+    });
+  });
+};
+
+// Selects every row from type_table, returns array of every type stored in table
+exports.getThesis_types = () => {
+  return new Promise((resolve, reject) => {
+    const sql =
+      `SELECT * FROM type_table`;
+      connection.query(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        const types = [];
+        rows.map((e) =>{
+          types.push(e.name);
+        })        
+        resolve(types);
+      };
+    });
+  });
+};
+
+// Selects every row from keyword table, returns array of all keywords stored in table
+exports.getKeywords = () => {
+  return new Promise((resolve, reject) => {
+    const sql =
+      `SELECT * FROM keyword`;
+      connection.query(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        const keywords = [];
+        rows.map((e) =>{
+          keywords.push(e.name);
+        })
+        resolve(keywords);
+      };
+    });
+  });
+};
+
+// Inserts new row  in keyword table, must receive a new keyword not already present in table, returns newly inserted row
+exports.createKeyword = (keyword) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'INSERT INTO keyword (name) VALUES (?)';
+    connection.query(sql, [keyword], function (err) {
+      if (err) {
+        reject(err);
+      }
+      resolve(keyword);
+    });
+  });
+};
+
+// Inserts in thesis_keyword table a new row, must receive thesis id and keyword, returns inserted row
+exports.createThesis_keyword = (thesis_id, keyword) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'INSERT INTO thesis_keyword (thesis_id, keyword_name) VALUES (?,?)';
+    connection.query(sql, [thesis_id, keyword], function (err) {
+      if (err) {
+        reject(err);
+      }
+      const thesis_keyword = {
+        thesis_id: thesis_id,
+        keyword: keyword
+      }
+      resolve(thesis_keyword);
+    });
+  });
+};
+
+// Selects every teacher id from teacher table, return array of teachers codes
+exports.getTeachers = () => {
+  return new Promise((resolve, reject) => {
+    const sql =
+      `SELECT id FROM teacher`;
+      connection.query(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        const teachers = [];
+        rows.map((e) =>{
+          teachers.push(e.id);
+        })
+        resolve(teachers);
+      };
+    });
+  });
+};
+
+// Selects every code of degrees from degree_table, returns array of degrees codes
+exports.getDegrees = () => {
+  return new Promise((resolve, reject) => {
+    const sql =
+      `SELECT cod_degree FROM degree_table`;
+      connection.query(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        const degrees = [];
+        rows.map((e) =>{
+          degrees.push(e.cod_degree);
+        })
+        resolve(degrees);
+      };
+    });
+  });
+};
+
+// Selects every code of research groups from group_table, returns array of codes
+exports.getCodes_group = () => {
+  return new Promise((resolve, reject) => {
+    const sql =
+      `SELECT cod_group FROM group_table`;
+      connection.query(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        const codes_group = [];
+        rows.map((e) =>{
+          codes_group.push(e.cod_group);
+        })
+        resolve(codes_group);
+      };
+    });
+  });
+};
+
+// Inserts new row in thesis_group table, must receive id of thesis and id of related group, returns new inserted row
+exports.createThesis_group = (thesis_id ,group_id) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'INSERT INTO thesis_group (thesis_id, group_id) VALUES (?,?)';
+    connection.query(sql, [thesis_id, group_id], function (err) {
+      if (err) {
+        reject(err);
+      }
+      const thesis_group ={ 
+        thesis_id: thesis_id,
+        group_id: group_id
+      }
+      resolve(thesis_group);
+    });
+  });
+};
+
+// Insert new row in thesis_cosupervisor table, must receive thesis id and cosupervisor id
+exports.createThesis_cosupervisor = (thesis_id ,cosupervisor_id) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'INSERT INTO thesis_cosupervisor (thesis_id, cosupevisor_id) VALUES (?,?)';
+    connection.query(sql, [thesis_id, cosupervisor_id], function (err) {
+      if (err) {
+        reject(err);
+      }
+      const thesis_cosupervisor ={ 
+        thesis_id: thesis_id,
+        thesis_cosupervisor: cosupervisor_id
+      }
+      resolve(thesis_cosupervisor);
+    });
+  });
+};
+
+// Selects every email from external_supervisor table, returns array of email
+exports.getExternal_cosupervisors = () => {
+  return new Promise((resolve, reject) => {
+    const sql =
+      `SELECT email FROM external_supervisor`;
+      connection.query(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        const external_supervisor_codes = [];
+        rows.map((e) =>{
+          external_supervisor_codes.push(e.email);
+        })
+        resolve(external_supervisor_codes);
+      };
+    });
+  });
+};
--- server/index.js
+++ server/index.js
@@ -110,3 +110,115 @@ app.get("/api/session/userinfo", (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+app.post('/api/newThesis', isProfessor, [
+  // Various checks of syntax of given data
+  check('title').isLength({ min: 1, max: 100 }),
+  check('supervisor_id').isLength({ min: 1, max: 7 }),
+  check('thesis_level').isIn(['bachelor', 'master']),
+  check('type_name').isLength({ min: 1, max: 50 }),
+  check('expiration').isISO8601().toDate().withMessage("Date time must be in forma YYYY-MM-DD HH:MM:SS"), // TODO check if given date is NOT earlier than today
+  check('cod_degree').isLength({ min: 1, max: 10 }),
+  check('is_archived').isBoolean()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors });
+  }
+
+  try {
+
+    // ---Professor should be able to insert whatever he wants as type of thesis
+    // Get every type of thesis from db
+    const thesis_types = await dao.getThesis_types();
+    // If the type of new thesis is not in list, add it
+    if (!thesis_types.includes(req.body.type_name)) {
+      const result_thesis_type = dao.createThesis_type(req.body.type_name);
+    }
+
+
+    // ---Professor should be able to insert any quantity of words as new keywords for a thesis
+    // Get every keyword from db 
+    const stored_keywords = await dao.getKeywords();
+    // For every keyword of new thesis, check if its already stored in list, if not add it
+    req.body.keywords.forEach(keyword => {
+      if (!stored_keywords.includes(keyword)) {
+        const result_keyword = dao.createKeyword(keyword);
+      }
+    });
+
+
+    // ---Main supervisor is a teacher, check if given supervisor_id is in teacher table, if it isn't raise error   
+    //Get every teacher id from teacher table
+    const teachers = await dao.getTeachers();
+    //Check if given teacher id is in list
+    if (!teachers.includes(req.body.supervisor_id)) {
+      return res.status(400).json({ error: `Supervisor_id: ${req.body.supervisor_id} is not a teacher` });
+    }
+
+    // ---Cod_degree should be an actual degree, must be in degree table
+    //Get every cod_degree from degree_table table in db
+    const degrees = await dao.getDegrees()
+    //If given cod_degre si not in list raise error
+    if (!degrees.includes(req.body.cod_degree)) {
+      return res.status(400).json({ error: `Cod_degree: ${req.body.cod_degree} is not a valid degree code` });
+    }
+
+    // --- Group_id should be and actual research group id, must be in group_table
+    // Get every cod_group from group_table table in db
+    const codes_group = await dao.getCodes_group();
+    // If given cod_group is not in list  raise error
+    if (!codes_group.includes(req.body.cod_group)) {
+      return res.status(400).json({ error: `Cod_group: ${req.body.cod_group} is not a valid research group code` });
+    }
+
+    const external_supervisor_codes = await dao.getExternal_cosupervisors();
+    for(const cosupervisor of req.body.cosupervisors){
+      if(! (external_supervisor_codes.includes(cosupervisor) || teachers.includes(cosupervisor))){
+        return res.status(400).json({ error: `Cosupervisor code: ${cosupervisor} is not a valid internal or external supervisor code` })
+      }
+    }
+
+    //Create thesis object which contains data from front end
+    const thesis = {
+      title: req.body.title,
+      description: req.body.description,
+      supervisor_id: req.body.supervisor_id,
+      thesis_level: req.body.thesis_level,
+      type_name: req.body.type_name,
+      required_knowledge: req.body.required_knowledge,
+      notes: req.body.notes,
+      expiration: req.body.expiration,
+      cod_degree: req.body.cod_degree,
+      is_archived: req.body.is_archived,
+    }
+    //Insert new thesis in db
+    const result_thesis = await dao.createThesis(thesis);
+
+    //Create a new thesis_keyword row which links thesis to its relative keywords
+    for (const keyword of req.body.keywords) {
+      const result_Thesis_keywords = [];
+      result_Thesis_keywords.push(await dao.createThesis_keyword(result_thesis.id, keyword));
+
+    };
+
+    //Create a new thesis_group row which links thesis to its research group
+     const result_thesis_group = await dao.createThesis_group(result_thesis.id, req.body.cod_group);
+/*
+    TODO AFTER DB FIXES
+
+    //Create new thesis cosupervisor
+    for (const cosupervisore of req.body.cosupervisors) {
+      const result_thesis_cosupervisor = [];
+      result_thesis_cosupervisor.push(await dao.createThesis_cosupervisor(result_thesis.id, cosupervisore))
+    }
+*/
+    //Return inserted data
+    return res.json(result_thesis);
+  } catch (err) {
+    return res.status(503).json({ error: ` error: ${err} ` });
+  }
+
+
+
+});
