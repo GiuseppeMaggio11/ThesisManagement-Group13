3e2c148676352642e16a288a1df08ac8d33ce947
Paolo Campagnale s317687 <s317687@studenti.polito.it>
2023-11-14 23:28:57 +0100

    Add unit test for isThesisValid

--- server/test/dao.integration.test.js
+++ server/test/dao.integration.test.js
@@ -1,3 +1,15 @@
+const dao = require("../dao");
+const mysql = require("mysql2");
+
+// open the database
+const dbConfig = {
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    database: "db_se_thesismanagement",
+  };
+const connection = mysql.createConnection(dbConfig);
+
 describe("Prova", () => {
     test("Prova1", () => {
         expect(true).toBe(true);
--- server/test/dao.unit.test.js
+++ server/test/dao.unit.test.js
@@ -1,5 +1,96 @@
-describe("Prova", () => {
-    test("Prova1", () => {
-        expect(true).toBe(true);
-    })
-})
+const dao = require("../dao");
+const mysql = require("mysql2");
+
+
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
+describe("isThesisValid", () => {
+    
+    test("Should return true if the thesis is not expired or archived", async () => {
+        const mockInput = {
+            thesisID: 1,
+            formattedDate: new Date().toISOString().slice(0, 19).replace("T", " ")
+        };
+        const mockExecute = jest.spyOn(mysql.Connection.prototype, "execute").mockResolvedValue(["thesis exists"]);
+
+        const result = await dao.isThesisValid(mockInput.thesisID, mockInput.formattedDate);
+        
+        /*expect(mockExecute).toHaveBeenCalledWith(
+            "SELECT * FROM thesis WHERE id = ? AND expiration>? AND is_archived = FALSE",
+            [
+                mockInput.thesisID, 
+                mockInput.formattedDate
+            ]
+        );*/
+        expect(mockExecute).toHaveBeenCalled();
+        expect(result).toBe(true);
+    });
+
+    test("Should return false if the thesis is expired or archived", async () => {
+        const mockInput = {
+            thesisID: 1,
+            formattedDate: new Date().toISOString().slice(0, 19).replace("T", " ")
+        };
+        const mockExecute = jest.spyOn(mysql.Connection.prototype, "execute").mockResolvedValue([]);
+
+        const result = await dao.isThesisValid(mockInput.thesisID, mockInput.formattedDate);
+
+        /*expect(mockExecute).toHaveBeenCalledWith(
+            "SELECT * FROM thesis WHERE id = ? AND expiration>? AND is_archived = FALSE",
+            [
+                mockInput.thesisID, 
+                mockInput.formattedDate
+            ]
+        );*/
+        expect(mockExecute).toHaveBeenCalled();
+        expect(result).toBe(false);
+    });
+
+    test("Should throw an error if \"thesisID\" parameter is missing", async () => {
+        const mockInput = {
+            thesisID: undefined,
+            formattedDate: new Date().toISOString().slice(0, 19).replace("T", " ")
+        };
+        const mockExecute = jest.spyOn(mysql.Connection.prototype, "execute");
+
+        await expect(dao.isThesisValid(mockInput.thesisID, mockInput.formattedDate)).rejects.toStrictEqual(
+            {
+                error: "parameter is missing"
+            }
+        );
+
+        expect(mockExecute).not.toHaveBeenCalled();
+    });
+
+    test.skip("Should throw an error if \"date\" parameter is missing", async () => {
+        const mockInput = {
+            thesisID: 1,
+            formattedDate: undefined
+        };
+        const mockExecute = jest.spyOn(mysql.Connection.prototype, "execute");
+
+        await expect(dao.isThesisValid(mockInput.thesisID, mockInput.formattedDate)).rejects.toStrictEqual(
+            {
+                error: "parameter is missing"
+            }
+        );
+
+        expect(mockExecute).not.toHaveBeenCalled();
+    });
+
+    test("Should handle errors during query execution", async () => {
+        const mockInput = {
+            thesisID: 1,
+            formattedDate: new Date().toISOString().slice(0, 19).replace("T", " ")
+        };
+        const mockExecute = jest.spyOn(mysql.Connection.prototype, "execute").mockRejectedValue('Database error');
+
+        await expect(dao.isThesisValid(mockInput.thesisID, mockInput.formattedDate)).rejects.toStrictEqual("Database error");
+        
+        expect(mockExecute).toHaveBeenCalled();
+    });
+});
+
