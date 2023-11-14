1d3bc4d1dc478b3d41b0a03206185e21f8e0007a
Matteo <matteo.busnelli10@gmail.com>
2023-11-14 22:39:00 +0100

    Style changes

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -48,7 +48,6 @@ function LoginForm(props) {
   return (
     <>
       <Container>
-        {props.loading ? <Loading /> : ""}
         <Row>
           <Col
             style={{
@@ -91,12 +90,12 @@ function LoginForm(props) {
                       onChange={(ev) => setPassword(ev.target.value)}
                     />
                   </Form.Group>
-                  <Button className="my-2" type="submit">
+                  <Button className="button-style my-2" type="submit">
                     Login
                   </Button>
                   <Button
-                    className="my-2 mx-2"
-                    variant="secondary"
+                    className="button-style-cancel my-2 mx-2"
+                    variant="light"
                     onClick={() => navigate("/")}
                   >
                     Back
