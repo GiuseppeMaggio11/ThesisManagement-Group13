936a669dc0ba767e4e523ca4e4b28aeb1da30211
GIUSEPPE MAGGIO s313346 <s313346@studenti.polito.it>
2023-11-17 22:16:01 +0100

    controllers folder

---
+++ server/controllers/showThesis.js
@@ -0,0 +1,28 @@
+const dao = require("../dao");
+const {validationResult} = require("express-validator");
+
+
+async function getProposals (req, res) {
+    try {
+        const proposals = await dao.getProposals(req.user.user_type, req.user.username, req.query.date);
+        res.status(200).json(proposals);
+      } catch (err) {
+        res.status(500).json({ error: ` error: ${err} ` });
+      }
+}
+
+async function getProposal (req,res){
+    const thesis_id = req.params.id; // Extract thesis_id from the URL
+    try {
+      if (!Number.isInteger(Number(thesis_id))) {
+        throw new Error('Thesis ID must be an integer');
+      }
+  
+      const proposal = await dao.getProposalById(thesis_id, req.user.user_type, req.user.username);
+      res.status(200).json(proposal);
+    } catch (error) {
+      res.status(500).send(error.message + ' ');
+    }
+}
+
+module.exports = {getProposals, getProposal}
