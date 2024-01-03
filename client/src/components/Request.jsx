3ea9a8d838b0d9c7113fcfeafedee540f2e07990
Manuel Fissore <manuel.fissore.37@gmail.com>
2024-01-03 13:06:05 +0100

    requests page

--- client/src/components/Request.jsx
+++ client/src/components/Request.jsx
@@ -1,17 +1,21 @@
 import { Button, Card, Col, Container, OverlayTrigger, Row } from "react-bootstrap";
 import { motion } from 'framer-motion';
 import { useEffect, useState } from "react";
-import { Calendar,Pencil } from "react-bootstrap-icons";
+import { Calendar, Pencil, X } from "react-bootstrap-icons";
 import dayjs from "dayjs";
 import Loading from "./Loading";
 import randomcolor from "randomcolor";
 import API from "../API";
-
-
+import NoFileFound from "./NoFileFound";
+import {
+    Check2,
+    XLg
+} from "react-bootstrap-icons";
+import { useMediaQuery } from "react-responsive";
 
 function RequestsPage(props) {
     const [requestList, setRequestList] = useState([])
-    
+    const isMobile = useMediaQuery({ maxWidth: 767 });
 
     useEffect(() => {
         async function getRquests() {
@@ -33,139 +37,175 @@ function RequestsPage(props) {
         props.setLoading(false)
     }, [props.user]);
 
-
-
-    /* return props.loading ? (
+    return props.loading ? (
         <Loading />
-      ) : (
-        <Container className="p-4">
-          <Row className="justify-content-between">
-            <Col xs={8} className="fs-2">
-              Requests
-            </Col>
-        </Row>
-        {
-            requestList?.map((req, index)=>{
-                <RequestCard index={index} request={req}/>
-            })
-        }  
+    ) : (
+        <Container className=" d-flex p-4">
+            <Row className="fs-2 mb-2">
+                Requests
+            </Row>
+            <Row style={{marginTop:'3em'}}>
+                {requestList.length > 0 ?
+                    (requestList?.map((req, index) => {
+                        return (
+                            <RequestCard key={index} request={req} isMobile={isMobile} isSecretary={props.user.user_type==='SECR'?true:false}/>
+                        )
+
+                    })) : <NoFileFound message={"No request found"} />
+                }
+            </Row>
         </Container>
-      ) */
-      return(
-        <h1>Requests</h1>
-      )
+    )
 }
 
-function RequestCard(props){
-    const {request} = props
-
-    return(
-    <Col xs={12} md={12} lg={12} xl={12} xxl={12} className="mt-4">
+function RequestCard(props) {
+    const { request, isMobile,isSecretary } = props
+    console.log(request)
+    return (
+        <Col xs={12} md={12} lg={6} xl={4} xxl={4} className="mt-4">
             <motion.div
                 whileHover={{ scale: 1.05 }}
                 style={{ cursor: 'pointer' }}
             >
-                <Card style={{ padding: 20 }} className="custom-card-proposals">
-                    <Row>
-                        <Col style={{ minWidth: "300px" }}>
-                            <div
-                                className="title-custom-proposals"
-                                /* onClick={() => navigate("/viewproposal/" + props.proposal.id)} */
+                <Card style={{ padding: 20, paddingTop: 15 }} className="custom-card-proposals">
+                    <Card.Header style={{ padding: 0, backgroundColor: 'white', borderBottom: 'none' }} >
+                        <Col className="text-end">
+                            <span
+                                className="badge"
                                 style={{
-                                    fontWeight: "medium",
-                                    fontSize: 20,
-                                    cursor: "pointer",
+                                    backgroundColor: randomcolor({
+                                        seed: request.student_id + (Number((request.student_id).slice(1)) / 3) * 7,
+                                        luminosity: "bright",
+                                        format: "rgba",
+                                        alpha: 1,
+                                    }).replace(/1(?=\))/, "0.1"),
+                                    color: randomcolor({
+                                        seed: request.student_id + (Number((request.student_id).slice(1)) / 3) * 7,
+                                        luminosity: "bright",
+                                        format: "rgba",
+                                        alpha: 1,
+                                    }),
+                                    padding: "0.5em 1.2em",
+                                    borderRadius: "0.25rem",
+                                    marginRight: 10,
                                 }}
                             >
-                                {request.title}
-                            </div>
+                                {request.student_id}
+                            </span>
+               
+                            {isSecretary && <span
+                                className="badge"
+                                style={{
+                                    backgroundColor: randomcolor({
+                                        seed: request.supervisor_id + (Number((request.supervisor_id).slice(1)) / 3) * 7,
+                                        luminosity: "bright",
+                                        format: "rgba",
+                                        alpha: 1,
+                                    }).replace(/1(?=\))/, "0.1"),
+                                    color: randomcolor({
+                                        seed: request.supervisor_id + (Number((request.supervisor_id).slice(1)) / 3) * 7,
+                                        luminosity: "bright",
+                                        format: "rgba",
+                                        alpha: 1,
+                                    }),
+                                    padding: "0.5em 1.2em",
+                                    borderRadius: "0.25rem",
+                                    marginRight: 10,
+                                }}
+                            >
+                                {request.supervisor_id}
+                            </span>}
+                        </Col> 
+                    </Card.Header>
+                    <Row>
+                        <Col xs={8}>
+                            <Row>
+                                <div
+                                    className="title-custom-proposals"
+                                    /* onClick={() => navigate("/viewproposal/" + request.id)} */
+                                    style={{
+                                        fontWeight: "medium",
+                                        fontSize: 20,
+                                        cursor: "pointer",
+                                    }}
+                                >
+                                    {request.title}
+                                </div>
+                            </Row>
+                            <Row>
+                                <Col xs={12} md={12} lg={12}>
+                                    <div
+                                        className="title-custom-proposals"
+                                        style={{
+                                            fontWeight: "medium",
+                                            fontSize: 15,
+                                            cursor: "pointer",
+                                        }}
+                                    >
+                                        <span>{'by '}</span>
+                                        <span style={{ fontWeight: 500 }}>{request.student_fullname}</span>
+                                    </div>
+                                </Col>
+                            </Row>
                         </Col>
                         <Col className="text-end mx-2">
-                            <Row>
-                                <Col xs={6} md={6} lg={3}>
-                                    <OverlayTrigger
+                            <Row style={{ marginTop: '1em' }}>
+                                <Col xs={6} md={4} lg={4} xl={6}>
+                                    {isMobile ? <Check2 style={{ fontSize: 20 }} /> :
+                                        <>
+                                            {/* <OverlayTrigger
                                         placement="bottom"
                                         delay={{ show: 250, hide: 400 }}
-                                        /* overlay={renderTooltipEdit} */
-                                    >
-                                        <Button
-                                            variant="light"
-                                          /*   onClick={() => {
-                                                navigate("/updateproposal/" + props.proposal.id);
-                                            }} */
-                                        >
-                                            {!props.isMobile && <span className="mx-2">Accept</span>}
-                                            {/* <Pencil /> */}
-                                        </Button>
-                                    </OverlayTrigger>
-                                </Col>
-                                <Col xs={6} md={6} lg={3}>
-                                    <OverlayTrigger
-                                        placement="bottom"
-                                        delay={{ show: 250, hide: 400 }}
-                                        overlay={renderTooltipCopy}
-                                    >
-                                        <Button
-                                            variant="light"
-                                            onClick={() => {
-                                                navigate("/copyproposal/" + props.proposal.id);
-                                            }}
-                                        >
-                                            {!props.isMobile && <span className="mx-2">Copy</span>}
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="16"
-                                                height="16"
-                                                fill="currentColor"
-                                                className="bi bi-copy"
-                                                viewBox="0 0 16 16"
+                                    /* overlay={renderTooltipEdit
+                                    > */}
+                                            <Button
+                                                variant="light"
+                                            /*  onClick={() => {
+                                                 navigate("/copyproposal/" + request.id);
+                                             }} */
                                             >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
-                                                />
-                                            </svg>
-                                        </Button>
-                                    </OverlayTrigger>
+                                                <Check2 />
+                                            </Button>
+                                            {/* </OverlayTrigger> */}
+                                        </>}
+
+
+
+                                </Col>
+                                <Col xs={6} md={4} lg={4} xl={6}>
+
+                                    {isMobile ? <XLg style={{ fontSize: 15 }} /> :
+                                        <>
+                                            {/*  <OverlayTrigger
+                                        placement="bottom"
+                                    overlay={renderTooltipCopy}
+                                    > */}
+                                            <Button
+                                                variant="light"
+                                            /*  onClick={() => {
+                                                 navigate("/copyproposal/" + request.id);
+                                             }} */
+                                            >
+                                                <XLg />
+                                            </Button>
+
+                                            {/* </OverlayTrigger> */}
+                                        </>
+                                    }
                                 </Col>
                             </Row>
                         </Col>
                     </Row>
+
                     <div
                         className="hide-scrollbar"
                         style={{
                             fontWeight: "semi-bold",
                             fontSize: 14,
-                            height: !props.isMobile ? 25 : 40,
+                            height: !isMobile ? 25 : 40,
                             marginTop: 5,
                         }}
                     >
-                        {props.proposal.keywords &&
-                            props.proposal.keywords.split(", ").map((key, index) => (
-                                <span
-                                    key={index}
-                                    className="badge"
-                                    style={{
-                                        backgroundColor: randomcolor({
-                                            seed: key,
-                                            luminosity: "bright",
-                                            format: "rgba",
-                                            alpha: 1,
-                                        }).replace(/1(?=\))/, "0.1"),
-                                        color: randomcolor({
-                                            seed: key,
-                                            luminosity: "bright",
-                                            format: "rgba",
-                                            alpha: 1,
-                                        }),
-                                        padding: "0.5em 1.2em",
-                                        borderRadius: "0.25rem",
-                                        marginRight: 10,
-                                    }}
-                                >
-                                    {key}
-                                </span>
-                            ))}
                     </div>
                     <div
                         style={{
@@ -174,23 +214,20 @@ function RequestCard(props){
                             minHeight: 50,
                         }}
                     >
-                        {props.proposal.description.length > (props.isMobile ? 100 : 600) ? (
+                        {request.description.length > (isMobile ? 100 : 600) ? (
                             <>
                                 <span>
-                                    {props.proposal.description.substring(
+                                    {request.description.substring(
                                         0,
-                                        props.isMobile ? 100 : 600
+                                        isMobile ? 100 : 600
                                     ) + "..... "}
                                 </span>
-                                <span
-                                    className="description-read-more"
-                                    onClick={() => navigate("/viewproposal/" + props.proposal.id)}
-                                >
+                                <span className="description-read-more">
                                     Read more
                                 </span>
                             </>
                         ) : (
-                            props.proposal.description
+                            request.description
                         )}
                     </div>
                     <Row
@@ -199,7 +236,7 @@ function RequestCard(props){
                             marginTop: 16,
                         }}
                     >
-                        <Col style={{ maxWidth: "110px" }}>
+                        <Col xs={6} md={6} lg={6}>
                             <span>Thesis Level</span>
                         </Col>
                         <Col>
@@ -209,7 +246,7 @@ function RequestCard(props){
                                 }}
                                 className="badge"
                             >
-                                {props.proposal.thesis_level.toUpperCase()}
+                                {request.thesis_level.toUpperCase()}
                             </span>
                         </Col>
                     </Row>
@@ -219,7 +256,7 @@ function RequestCard(props){
                             marginTop: 16,
                         }}
                     >
-                        <Col style={{ maxWidth: "110px" }}>
+                        <Col xs={6} md={6} lg={6}>
                             <span>Thesis Type</span>
                         </Col>
                         <Col>
@@ -229,7 +266,7 @@ function RequestCard(props){
                                 }}
                                 className="badge"
                             >
-                                {props.proposal.thesis_type.toUpperCase()}
+                                {request.thesis_type.toUpperCase()}
                             </span>
                         </Col>
                     </Row>
@@ -239,14 +276,13 @@ function RequestCard(props){
                             marginTop: 16,
                         }}
                     >
-                        <Col style={{ maxWidth: "110px" }}>
-                            <span>Expire at</span>
+                        <Col xs={6} md={6} lg={6}>
+                            <span>Code Degree</span>
                         </Col>
                         <Col>
                             <span className="badge" style={{ color: "black" }}>
-                                {dayjs(props.proposal.expiration).format("MM/DD/YYYY")}
+                                {request.cod_degree}
                             </span>
-                            <Calendar />
                         </Col>
                     </Row>
                 </Card>
